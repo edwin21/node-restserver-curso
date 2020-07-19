d26fea0d7c4866c0c827d8f0583e95148684adfc
Edwin <edwin21hernandez@gmail.com>
2020-07-19 10:41:23 -0700

    RestServer - usuario

--- server/config/config.js
+++ server/config/config.js
@@ -5,3 +5,25 @@
 
 
 process.env.PORT = process.env.PORT || 3000;
+
+//==============
+//Entorno
+//==============
+
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+//==============
+//Base de datos
+//==============
+
+let urlDB;
+
+if (process.env.NODE_ENV === 'dev') {
+
+    urlDB = 'mongodb://localhost:27017/cafe';
+} else {
+    urlDB = 'mongodb+srv://cursonode:wH0Ao3F3euFO7cUg@cluster0.rhetn.mongodb.net/cafe';
+}
+
+
+process.env.URLDB = urlDB;
